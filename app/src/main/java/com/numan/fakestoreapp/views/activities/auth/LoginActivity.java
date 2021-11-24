806ef5e5d6d07da57a2718e38797e567a02776a4
Numan Qamar <numanqmr@gmail.com>
2021-11-24 23:50:45 +0500

    login API integration complete with credentials validation.

--- app/src/main/java/com/numan/fakestoreapp/views/activities/auth/LoginActivity.java
+++ app/src/main/java/com/numan/fakestoreapp/views/activities/auth/LoginActivity.java
@@ -10,6 +10,8 @@ import com.google.android.material.snackbar.Snackbar;
 import com.numan.fakestoreapp.R;
 import com.numan.fakestoreapp.common.CloseSoftKeyBoardOnTouchOutside;
 import com.numan.fakestoreapp.common.ConnectionManager;
+import com.numan.fakestoreapp.common.InputValidator;
+import com.numan.fakestoreapp.common.MySharedPreference;
 import com.numan.fakestoreapp.common.dtos.Login;
 import com.numan.fakestoreapp.databinding.ActivityLoginBinding;
 import com.numan.fakestoreapp.views.activities.BaseActivity;
@@ -42,30 +44,31 @@ public class LoginActivity extends BaseActivity {
     private void initViews(ActivityLoginBinding binding) {
 
         binding.btnLogin.setOnClickListener(v -> {
-            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-            finish();
-            //TODO: use input validators
-            if (!TextUtils.isEmpty(binding.etEmail.getText()) && !TextUtils.isEmpty(binding.etPassword.getText())) {
-
-                if (ConnectionManager.isOnline(getApplicationContext())) {
-                    showProgress(this);
-                    Login login = new Login();
-                    mViewModel.loginUser(login);
-                } else {
-                    try {
-                        Snackbar.make(findViewById(android.R.id.content), "You're offline. Please connect to an internet connection.", Snackbar.LENGTH_INDEFINITE)
-                                .setAction("CLOSE", view -> {
-
-                                })
-                                .setActionTextColor(getResources().getColor(android.R.color.white))
-                                .show();
-                    } catch (NullPointerException | IllegalStateException e) {
-                        e.printStackTrace();
-                    }
-                }
-
+//            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+//            finish();
+            if (!InputValidator.isValidEmail(binding.etEmail.getText().toString())) {
+                showToast("Please enter a valid Email!");
+            }else if(!InputValidator.isValidPassword(binding.etPassword.getText().toString())) {
+                showToast("Please enter a valid password, password must be min of 8 characters!");
             } else {
-                //TODO error handling
+            if (ConnectionManager.isOnline(getApplicationContext())) {
+                showProgress(this);
+                Login login = new Login();
+                login.setEmail(binding.etEmail.getText().toString());
+                login.setPassword(binding.etPassword.getText().toString());
+                mViewModel.loginUser(login);
+            } else {
+                try {
+                    Snackbar.make(findViewById(android.R.id.content), "You're offline. Please connect to an internet connection.", Snackbar.LENGTH_INDEFINITE)
+                            .setAction("CLOSE", view -> {
+
+                            })
+                            .setActionTextColor(getResources().getColor(android.R.color.white))
+                            .show();
+                } catch (NullPointerException | IllegalStateException e) {
+                    e.printStackTrace();
+                }
+            }
             }
         });
 
@@ -82,12 +85,12 @@ public class LoginActivity extends BaseActivity {
 
             if (loginResponse != null) {
 
-                //MySharedPreference.setUser(getApplicationContext(), loginResponse);
+                MySharedPreference.setUser(getApplicationContext(), loginResponse);
                 startActivity(new Intent(LoginActivity.this, MainActivity.class));
                 finish();
 
             } else {
-                //TODO error handling
+                showToast("Unable to login, please try again later.");
             }
         });
 
